327469fed4d9584eb2e3e7a00dbb4cfca893ca5c
sagarcb <sagar.cb.009@gmail.com>
2021-11-03 21:24:07 +0600

    menu item created

---
+++ src/data/dishes.js
@@ -0,0 +1,82 @@
+export const DISHES = [
+    {
+        id: 0,
+        name: 'Ginger Burger',
+        image: 'assets/images/burger.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: 'adfja asdfj;ads faksdfj;adsf ad;fakldfj;adfkja;dlfkajds fkadjf;a' +
+            'asdfljadsfkla;dflkadf; akdfja ajakd adksjf;a fadsfkj',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: 'Great food',
+                author: 'Sagar',
+                date: '2020-10-16'
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Chicken Bar B Q Pizza',
+        image: 'assets/images/bbqpizza.jpg',
+        category: 'meal',
+        label: '',
+        price: '599',
+        description: 'aldksfjal kasfdja aldfkj akdfj aldfsjadf aldfjadlkf aldfjaldfjaldfjaldf' +
+            'ajdfladf ljadfkadlfafadf',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: 'Imagene all the eatables, living in confusion',
+                author: 'Sagar',
+                date: '2018-2-23'
+            }
+        ]
+
+    },
+    {
+        id: 1,
+        name: 'Chicken Masala',
+        image: 'assets/images/chickenmasala.jpg',
+        category: 'meal',
+        label: '',
+        price: '599',
+        description: 'aldksfjal kasfdja aldfkj akdfj aldfsjadf aldfjadlkf aldfjaldfjaldfjaldf' +
+            'ajdfladf ljadfkadlfafadf',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: 'Imagene all the eatables, living in confusion',
+                author: 'Sagar',
+                date: '2018-2-23'
+            }
+        ]
+
+    },
+    {
+        id: 3,
+        name: 'Chicken Biriyani',
+        image: 'assets/images/biriyani.jpg',
+        category: 'meal',
+        label: '',
+        price: '599',
+        description: 'aldksfjal kasfdja aldfkj akdfj aldfsjadf aldfjadlkf aldfjaldfjaldfjaldf' +
+            'ajdfladf ljadfkadlfafadf',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: 'Imagene all the eatables, living in confusion',
+                author: 'Sagar',
+                date: '2018-2-23'
+            }
+        ]
+
+    },
+
+]
